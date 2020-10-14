d8fe1c7c345bc3d401fb64f6893520a155ced313
paulami.kayal <paulami.kayal@P2CLAP-F1BJ9UT.dir.svc.accenture.com>
2020-10-14 10:42:25 +0530

    first commit to public repo

---
+++ Object Repository/tcobjec.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tcobjec</name>
+   <tag></tag>
+   <elementGuidId>a3d2dd25-c5e2-4283-992b-ded066872896</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
